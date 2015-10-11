f6fe985fb6aad2807ffd859f32479dd189fd83d2
Ile Eftimov <ileeftimov@gmail.com>
2015-10-11 03:42:44 +0200

    add package definition

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule EightBall.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package ]
   end
 
   # Configuration for the OTP application
@@ -28,5 +29,14 @@ defmodule EightBall.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README", "LICENSE*"],
+      maintainers: ["Ilija Eftimov"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/fteem/eight_ball"}
+    ]
   end
 end
